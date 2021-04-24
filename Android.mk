e7b4984d89adbb2f9494ec54bafa6e0cfaf2bc40
MSe1969 <mse1969@posteo.de>
2021-04-24 19:34:34 +0200

    Initial commit - setup repository

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Prebuilt Huawei Camera for Huawei treble build
+# ----------------------------------------------
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := HWCamera2
+LOCAL_SRC_FILES := HwCamera2.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
+
+# Watermark
+$(shell mkdir -p $(TARGET_OUT)/watermark/wm)
+$(shell cp $(LOCAL_PATH)/watermark/wm/* $(TARGET_OUT)/watermark/wm)
+
+# Camera plugins
+$(shell mkdir -p $(TARGET_OUT)/cameraplugins)
+$(shell cp -r $(LOCAL_PATH)/cameraplugins/* $(TARGET_OUT)/cameraplugins)
+
+# Local jni libraries
+$(shell mkdir -p $(TARGET_OUT)/priv-app/HWCamera2/lib/arm)
+$(shell cp -r $(LOCAL_PATH)/lib/arm/* $(TARGET_OUT)/priv-app/HWCamera2/lib/arm)
+
+# Links
+$(shell ln -s /system/lib/vndk-26/android.hidl.base@1.0.so $(TARGET_OUT)/priv-app/HWCamera2/lib/arm/android.hidl.base@1.0.so)
