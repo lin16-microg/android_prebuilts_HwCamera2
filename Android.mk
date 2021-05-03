205e561e72d8640c017b417150401c4a91f6e864
MSe1969 <mse1969@posteo.de>
2021-05-03 23:12:07 +0200

    HwCamera2: Add priv-app permissions whitelist
    Change-Id: I08e78d40eaf681ddfe39f36cf896431a40e6a3cf

--- Android.mk
+++ Android.mk
@@ -5,6 +5,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-HwCamera2.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := etc/permissions/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_OVERRIDES_PACKAGES := Camera2
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := HWCamera2
@@ -13,6 +21,7 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := privapp-permissions-HwCamera2.xml
 include $(BUILD_PREBUILT)
 
 # Watermark
