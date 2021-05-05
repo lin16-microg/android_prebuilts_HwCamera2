6cb7e065ba722944bbef1f702acd4b7546d8666e
MSe1969 <mse1969@posteo.de>
2021-05-06 01:12:56 +0200

    Android.mk - Optimize / reorganize Huawei camera build
     * Fix package name (HWCamera2 -> HwCamera2)
    
     * Remove $(shell ) commands, which are ALWAYS executed, even if the
       module isn't specified (bad practice). Instead use post install
       command
    
    Change-Id: Ie195580b347298936a12d6dc87cdb632ad41f343

--- Android.mk
+++ Android.mk
@@ -15,26 +15,19 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_OVERRIDES_PACKAGES := Camera2
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := HWCamera2
+LOCAL_MODULE := HwCamera2
 LOCAL_SRC_FILES := HwCamera2.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := privapp-permissions-HwCamera2.xml
+LOCAL_POST_INSTALL_CMD := \
+    mkdir -p $(TARGET_OUT)/watermark/wm; \
+    cp $(LOCAL_PATH)/watermark/wm/* $(TARGET_OUT)/watermark/wm; \
+    mkdir -p $(TARGET_OUT)/cameraplugins; \
+    cp -r $(LOCAL_PATH)/cameraplugins/* $(TARGET_OUT)/cameraplugins; \
+    mkdir -p $(TARGET_OUT)/priv-app/HwCamera2/lib/arm; \
+    cp -r $(LOCAL_PATH)/lib/arm/* $(TARGET_OUT)/priv-app/HwCamera2/lib/arm; \
+    ln -sf /system/lib/vndk-26/android.hidl.base@1.0.so $(TARGET_OUT)/priv-app/HwCamera2/lib/arm/android.hidl.base@1.0.so
 include $(BUILD_PREBUILT)
-
-# Watermark
-$(shell mkdir -p $(TARGET_OUT)/watermark/wm)
-$(shell cp $(LOCAL_PATH)/watermark/wm/* $(TARGET_OUT)/watermark/wm)
-
-# Camera plugins
-$(shell mkdir -p $(TARGET_OUT)/cameraplugins)
-$(shell cp -r $(LOCAL_PATH)/cameraplugins/* $(TARGET_OUT)/cameraplugins)
-
-# Local jni libraries
-$(shell mkdir -p $(TARGET_OUT)/priv-app/HWCamera2/lib/arm)
-$(shell cp -r $(LOCAL_PATH)/lib/arm/* $(TARGET_OUT)/priv-app/HWCamera2/lib/arm)
-
-# Links
-$(shell ln -sf /system/lib/vndk-26/android.hidl.base@1.0.so $(TARGET_OUT)/priv-app/HWCamera2/lib/arm/android.hidl.base@1.0.so)
